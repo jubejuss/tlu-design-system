66aac5699a6a7b6810c2195845998a14fc1b7033
Juho Kalberg <juss@vilenkell.eu>
2023-04-21 10:44:48 +0300

    First setup ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,32 @@
-const { filterTokensByType } = require("./fns");
-const lightTokens = require("./output/light.json")
-const darkTokens = require("./output/dark.json")
-const globalTokens = require("./output/global.json")
+const { filterTokensByType } = require('./fns');
+const tokens = require('./output/light.json');
+const globalTokens = require('./output/global.json');
 
-const lightColors = filterTokensByType('color', lightTokens)
-const darkColors = filterTokensByType('color', darkTokens)
-const globalColors = filterTokensByType('color', globalTokens)
+const colors = filterTokensByType('color', tokens);
+const globalColors = filterTokensByType('color', globalTokens);
 
-const baseFontSize = 1.125; // Base font size in rem's. If browser default size 16px, then now it is 18px
-const scaleRatio = 1.25; // Modular scale ratio
-const modularScale = (step) => `${(baseFontSize * Math.pow(scaleRatio, step)).toFixed(2)}rem`;
+const plugin = require('tailwindcss/plugin'); // for adding base styles
 
-/** @type {import('tailwindcss').Config} */
+const baseFontSize = 1; // Base font size in rem's.
+// Next is for fetching scaleRatio value from global.json
+const scaleRatio = parseFloat(
+  globalTokens.find((variable) => variable.name === 'font-scale-multiplyer')
+    .value
+);
+
+const modularScale = (step) =>
+  `${(baseFontSize * Math.pow(scaleRatio, step)).toFixed(2)}rem`;
+
 module.exports = {
-  content: ['./src/**/*.{html,js}', './src/index.html'],
-  darkMode: 'class', 
+  content: ['./src/**/*.{html,js}', './src/index.html', 'index.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontSize: {
-      'xs': modularScale(-2),
-      'sm': modularScale(-1),
-      'base': modularScale(0),
-      'lg': modularScale(1),
-      'xl': modularScale(2),
+      xs: modularScale(-2),
+      sm: modularScale(-1),
+      base: modularScale(0),
+      lg: modularScale(1),
+      xl: modularScale(2),
       '2xl': modularScale(3),
       '3xl': modularScale(4),
       '4xl': modularScale(5),
@@ -30,11 +35,44 @@ module.exports = {
       '7xl': modularScale(8),
     },
     colors: {
-      ...globalColors,
-      light: lightColors,
-      dark: darkColors // adding dark theme colors under a "dark" key
-    }
+      ...globalColors, // tokens from clobal.json
+      ...colors, // tokens from light.json (they are switchable with dark.json)
+    },
   },
   variants: {},
-  plugins: [],
-}
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        html: {
+          fontSize: '18px',
+          fontFamily: 'Montserrat',
+        },
+        h1: {
+          fontSize: theme('fontSize.4xl'),
+          fontWeight: '800',
+          fontFamily: 'Roboto Serif',
+        },
+        h2: {
+          fontSize: theme('fontSize.3xl'),
+          fontWeight: '700',
+          fontFamily: 'Roboto Serif',
+        },
+        h3: {
+          fontSize: theme('fontSize.2xl'),
+          fontWeight: '700',
+        },
+        h4: {
+          fontSize: theme('fontSize.xl'),
+          fontWeight: '700',
+        },
+        h5: {
+          fontSize: theme('fontSize.lg'),
+          fontWeight: '700',
+        },
+        h6: {
+          fontWeight: '700',
+        },
+      });
+    }),
+  ],
+};
